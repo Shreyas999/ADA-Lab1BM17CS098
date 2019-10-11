bf4c8d3d37c92e3c30c5a4e3616bc678f12528ac
Shreyas999 <53894147+Shreyas999@users.noreply.github.com>
2019-10-11 22:02:21 +0530

    Update 6a_nqueens.cpp

--- 6a_nqueens.cpp
+++ 6a_nqueens.cpp
@@ -1,57 +1,70 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-#define N 8
-void printBoard(int board[N][N]) {
-   for (int i = 0; i < N; i++) {
-      for (int j = 0; j < N; j++)
-         cout << board[i][j] << " ";
-         cout << endl;
-   }
+#define N 10
+
+void disp(int a[N][N],int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++)
+            cout << a[i][j] << " ";
+        cout <<"\n";
+    }
 }
-bool isValid(int board[N][N], int row, int col) {
-   for (int i = 0; i < col; i++)
-      if (board[row][i])
-         return false;
-   for (int i=row, j=col; i>=0 && j>=0; i--, j--)
-      if (board[i][j])
-         return false;
-   for (int i=row, j=col; j>=0 && i<N; i++, j--)
-      if (board[i][j])
-         return false;
-   return true;
+bool isValid(int a[N][N],int r,int c,int n) {
+    for (int i = 0; i < c; i++)
+        if (a[r][i])
+            return false;
+    for (int i=r, j=c; i>=0 && j>=0; i--, j--)
+        if (a[i][j])
+            return false;
+    for (int i=r, j=c; j>=0 && i<n; i++, j--)
+        if (a[i][j])
+            return false;
+    return true;
 }
-bool solveNQueen(int board[N][N], int col) {
-   if (col >= N)
-      return true;
-   for (int i = 0; i < N; i++) {
-      if (isValid(board, i, col) ) {
-         board[i][col] = 1;
-         if ( solveNQueen(board, col + 1))
+
+bool NQueen(int a[N][N], int c,int n) {
+    if (c >= n)
+        return true;
+    for (int i = 0; i < n; i++) {
+        if (isValid(a,i,c,n) ) {
+            a[i][c] = 1;
+        if (NQueen(a,c+1,n))
             return true;
-         board[i][col] = 0;
-      }
-   }
-   return false;
+        a[i][c] = 0;
+        }
+    }
+    return false;
 }
-bool checkSolution() {
-   int board[N][N];
-   for(int i = 0; i<N; i++)
-   for(int j = 0; j<N; j++)
-   board[i][j] = 0;
-   if ( solveNQueen(board, 0) == false ) {
-      cout << "Solution does not exist";
-      return false;
-   }
-   printBoard(board);
-   return true;
+
+bool check(int n) {
+    int a[N][N];
+    for(int i = 0; i<n; i++)
+    for(int j = 0; j<n; j++)
+    a[i][j] = 0;
+    if (NQueen(a,0,n) == false ) {
+        cout << "No Solution";
+        return false;
+    }
+    disp(a,n);
+    return true;
 }
 int main() {
-   checkSolution();
+    clock_t start,end;
+	double time;
+    int n;
+    cout<<"Enter no. of Queens (<=10): ";
+    cin>>n;
+    cout<<"\n";
+    start=clock();
+    check(n);
+    end=clock();
+	time=(double(end)-double(start))/double(CLOCKS_PER_SEC);
+	cout<<"\nTime Taken = "<<time<<"s\n";
 }
 
+/*OUTPUT
+Enter no. of Queens (<=10): 8
 
-/*
-OUTPUT
 1 0 0 0 0 0 0 0
 0 0 0 0 0 0 1 0
 0 0 0 0 1 0 0 0
@@ -60,4 +73,6 @@ OUTPUT
 0 0 0 1 0 0 0 0
 0 0 0 0 0 1 0 0
 0 0 1 0 0 0 0 0
+
+Time Taken = 0.012s
 */
