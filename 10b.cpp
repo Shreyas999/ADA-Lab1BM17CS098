320946a000c31ae0272b2c37844d44fdcbef39b3
Shreyas999 <53894147+Shreyas999@users.noreply.github.com>
2019-11-23 05:59:48 +0530

    Update 10b.cpp

--- 10b.cpp
+++ 10b.cpp
@@ -1,15 +1,12 @@
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 using namespace std; 
+int graph[10][10],V,dist[10][10];
+void printSolution(); 
 
-#define V 4 
-#define INF 99999 
-
-void printSolution(int dist[][V]); 
-
-void floyd(int graph[][V]) 
+void floyd() 
 { 
 	
-	int dist[V][V], i, j, k; 
+	int  i, j, k; 
 
 	
 	for (i = 0; i < V; i++) 
@@ -30,10 +27,10 @@ void floyd(int graph[][V])
 		} 
 	} 
 
-	printSolution(dist); 
+	printSolution(); 
 } 
 
-void printSolution(int dist[][V]) 
+void printSolution() 
 { 
 	cout<<"The following matrix shows the shortest distances"
 			" between every pair of vertices \n"; 
@@ -41,7 +38,7 @@ void printSolution(int dist[][V])
 	{ 
 		for (int j = 0; j < V; j++) 
 		{ 
-			if (dist[i][j] == INF) 
+			if (dist[i][j] == 9999) 
 				cout<<"INF"<<"	 "; 
 			else
 				cout<<dist[i][j]<<"	 "; 
@@ -52,21 +49,25 @@ void printSolution(int dist[][V])
 
 int main() 
 { 
-
-	int graph[V][V] = { {0, 5, INF, 10}, 
-						{INF, 0, 3, INF}, 
-						{INF, INF, 0, 1}, 
-						{INF, INF, INF, 0} 
-					}; 
-
-	floyd(graph); 
+    cout<<"Enter the number of vertices : ";
+    cin>>V;
+    cout<<"Enter the matrix :"<<endl;
+    for(int i=0;i<V;i++)
+        for(int j=0;j<V;j++)
+            cin>>graph[i][j];
+	floyd(); 
 	return 0; 
 } 
-/*
-OUTPUT
-The following matrix shows the shortest distances between every pair of vertices 
-0	    5	     8	    9	 
-INF	  0	     3	    4	 
-INF	  INF	   0	    1	 
-INF	  INF	   INF	  0	
+/*OUTPUT
+Enter the number of vertices : 4                                                                                                             
+Enter the matrix :                                                                                                                           
+0 9999 3 9999                                                                                                                                
+2 0 9999 9999                                                                                                                                
+9999 7 0 1                                                                                                                                   
+6 9999 9999 0                                                                                                                                
+The following matrix shows the shortest distances between every pair of vertices                                                             
+0        10      3       4                                                                                                                   
+2        0       5       6                                                                                                                   
+7        7       0       1                                                                                                                   
+6        16      9       0
 */
